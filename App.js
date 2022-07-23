839d3f7cf82d011f1fa02aa58a92bec3dee99109
Muhittin Orhan <101992606+muhittinorhan@users.noreply.github.com>
2022-07-23 22:27:25 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Components from "./Components"; // Components'i gösterebilmek için import ediyoruz.
+
+function App() {
+  return (
+    <div className="App">
+      <Components /> {/* import ettiğimiz Components'i gösteriyoruz. */}
+    </div>
+  );
+}
+
+export default App;
